7740e4588bcddb01e01915023e9f8f5f03f3ab04
bisre1921 <bisrattewodros3@gmail.com>
2024-06-27 17:38:29 +0300

    implement react router

--- Online-Shop/frontend/src/App.jsx
+++ Online-Shop/frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Container } from 'react-bootstrap'
+import { Outlet } from 'react-router-dom'
 import Header from './components/Header'
 import Footer from './components/Footer'
 import Home from './pages/Home'
@@ -10,7 +11,8 @@ const App = () => {
       <Header />
       <main className='py-3'>
         <Container>
-          <Home />
+          {/* <Home /> */}
+          <Outlet />
         </Container>
       </main>
       <Footer />
