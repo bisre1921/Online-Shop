9b54b30a906617cfeec5f130f7b18a9278bae44c
bisre1921 <bisrattewodros3@gmail.com>
2024-07-02 16:17:04 +0300

    redux store and api slices

--- Online-Shop/frontend/src/main.jsx
+++ Online-Shop/frontend/src/main.jsx
@@ -7,6 +7,8 @@ import './assets/styles/index.css'
 import './assets/styles/bootstrap.custom.css'
 import Home from './pages/Home.jsx';
 import ProductDetail from './pages/ProductDetail.jsx';
+import {Provider} from "react-redux";
+import store from "./store.jsx";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -19,6 +21,8 @@ const router = createBrowserRouter(
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
   </React.StrictMode>,
 )
