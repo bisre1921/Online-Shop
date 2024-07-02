d896a24d993fbf4c7229fd0268e8caa9b6d57d48
bisre1921 <bisrattewodros3@gmail.com>
2024-07-02 16:49:41 +0300

    products api slice and get products endpoint

--- Online-Shop/frontend/src/pages/Home.jsx
+++ Online-Shop/frontend/src/pages/Home.jsx
@@ -1,43 +1,18 @@
-import React, { useEffect, useState } from 'react';
 import { Row, Col } from 'react-bootstrap';
 import Product from '../components/Product';
-import axios from 'axios';
+import { useGetProductsQuery } from '../slices/productsApiSlice';
 
 const Home = () => {
-    const [products, setProducts] = useState([]);
-    const [loading, setLoading] = useState(true);
-    const [error, setError] = useState(null);
-
-    useEffect(() => {
-        const fetchProducts = async () => {
-            try {
-                const { data } = await axios.get('http://localhost:5000/api/products');
-                // const response = await fetch('http://localhost:5000/api/products');
-                // const data = await response.json();
-
-                if (Array.isArray(data)) {
-                    setProducts(data);
-                } else {
-                    throw new Error('Expected an array of products');
-                }
-            } catch (error) {
-                setError(error.message);
-            } finally {
-                setLoading(false);
-            }
-        };
-
-        fetchProducts();
-    }, []);
-
-    if (loading) return <div>Loading...</div>;
-    if (error) return <div>Error: {error}</div>;
+    const {data: products, error, isLoading} = useGetProductsQuery();
 
     return (
         <>
+            {isLoading && <h2>Loading...</h2>}
+            {error && <h2> {error?.data?.message || error?.error} </h2>}
+
             <h1>Latest Products</h1>
             <Row>
-                {products.map((product) => (
+                {products?.map((product) => (
                     <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
                         <Product product={product} />
                     </Col>
