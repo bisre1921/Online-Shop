611702d6cd09e631c12ee2ea7d7a79f5262f3191
bisre1921 <bisrattewodros3@gmail.com>
2024-07-03 14:15:09 +0300

    implement remove from cart functionality

--- Online-Shop/frontend/src/pages/Cart.jsx
+++ Online-Shop/frontend/src/pages/Cart.jsx
@@ -3,7 +3,7 @@ import {useDispatch , useSelector} from 'react-redux';
 import {Row , Col , ListGroup , Image , Form , Button , Card} from 'react-bootstrap';
 import {FaTrash} from 'react-icons/fa';
 import Message from '../components/Message';
-import { addToCart } from '../slices/CartSlice';
+import { addToCart , removeFromCart } from '../slices/CartSlice';
 
 const Cart = () => {
     const navigate = useNavigate();
@@ -14,6 +14,14 @@ const Cart = () => {
 
     const addToCartHandler = async (product , qty) => {
         dispatch(addToCart({...product , qty}));
+    }
+
+    const removeFromCartHandler = async (id) => {
+        dispatch(removeFromCart(id));
+    };
+
+    const checkoutHandler = () => {
+        navigate("/login?redirect=/shipping");
     }
 
   return (
@@ -49,7 +57,7 @@ const Cart = () => {
                                         </Form.Control>
                                     </Col>
                                     <Col md={2}>
-                                        <Button type="button" variant="light" onClick={() => removeFromCartHandler(item.product)}>
+                                        <Button type="button" variant="light" onClick={() => removeFromCartHandler(item._id)}>
                                             <FaTrash />
                                         </Button>
                                     </Col>
@@ -67,7 +75,7 @@ const Cart = () => {
                             ${cartItems.reduce((acc , item) => acc + item.qty * item.price , 0).toFixed(2)} 
                         </ListGroup.Item>
                         <ListGroup.Item>
-                            <Button type="button" className="btn-block" disabled={cartItems.length === 0} >
+                            <Button type="button" className="btn-block" disabled={cartItems.length === 0} onClick={checkoutHandler} >
                                 Proceed To Checkout
                             </Button>
                         </ListGroup.Item>
