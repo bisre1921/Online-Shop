6038e39a4fc47fd34de79edaab3b2e2c78172b2a
bisre1921 <bisrattewodros3@gmail.com>
2024-07-07 19:10:57 +0300

    implement login functionality

--- Online-Shop/frontend/src/App.jsx
+++ Online-Shop/frontend/src/App.jsx
@@ -4,6 +4,8 @@ import { Outlet } from 'react-router-dom'
 import Header from './components/Header'
 import Footer from './components/Footer'
 import Home from './pages/Home'
+import {ToastContainer} from "react-toastify"
+import 'react-toastify/dist/ReactToastify.css'
 
 const App = () => {
   return (
@@ -16,6 +18,7 @@ const App = () => {
         </Container>
       </main>
       <Footer />
+      <ToastContainer />
     </>
     
   )
